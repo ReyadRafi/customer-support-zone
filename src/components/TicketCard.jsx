a76370b20f5ca1c0af00b69f14573ae590f2b34b
Reyad Rafi <reyadrafi009v2@gmail.com>
2025-11-07 22:37:43 +0600

    text color correction

--- src/components/TicketCard.jsx
+++ src/components/TicketCard.jsx
@@ -1,16 +1,34 @@
 import React from "react";
 
 export default function TicketCard({ ticket, onSelect }) {
+  const getPriorityColor = (priority) => {
+    switch (priority.toLowerCase()) {
+      case "high":
+        return { color: "#F83044" }; 
+      case "medium":
+        return { color: "#dab148ff" }; 
+      case "low":
+        return { color: "#02A53B" }; 
+      default:
+        return { color: "#555" }; 
+    }
+  };
+
   return (
     <div className="ticket-card card" onClick={() => onSelect(ticket)}>
       <div className="ticket-header">
         <h4>{ticket.title}</h4>
         <span className="badge green">Open</span>
       </div>
+
       <p className="ticket-desc">{ticket.description}</p>
+
       <div className="ticket-meta">
         <span className="meta">
-          #{ticket.id} • {ticket.priority} Priority
+          #{ticket.id} •{" "}
+          <span style={getPriorityColor(ticket.priority)}>
+            {ticket.priority} Priority
+          </span>
         </span>
         <span className="meta">{ticket.customer}</span>
         <span className="meta">{ticket.createdAt}</span>
