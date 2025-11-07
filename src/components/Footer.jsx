826c7bdd77a35cd14f53f5641135b06412e19a65
Reyad Rafi <reyadrafi009v2@gmail.com>
2025-11-07 11:09:36 +0600

    update footer style

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -13,27 +13,35 @@ export default function Footer() {
         </div>
         <div className="footer-links">
           <div>
-            <h5>Company</h5>
-            <ul>
+            <h5 className="footer-title">Company</h5>
+            <ul className="footer-subtitle">
               <li>About Us</li>
               <li>Our Mission</li>
               <li>Contact</li>
             </ul>
           </div>
           <div>
-            <h5>Services</h5>
-            <ul>
+            <h5 className="footer-title">Services</h5>
+            <ul className="footer-subtitle">
               <li>Products</li>
               <li>Customer Stories</li>
               <li>Download Apps</li>
             </ul>
           </div>
           <div>
-            <h5>Information</h5>
-            <ul>
+            <h5 className="footer-title">Information</h5>
+            <ul className="footer-subtitle">
               <li>Privacy Policy</li>
               <li>Terms & Conditions</li>
               <li>Join Us</li>
+            </ul>
+          </div>
+          <div>
+            <h5 className="footer-title">Connect Us</h5>
+            <ul className="footer-subtitle">
+              <li>Facebook</li>
+              <li>Twitter</li>
+              <li>Instagram</li>
             </ul>
           </div>
         </div>
